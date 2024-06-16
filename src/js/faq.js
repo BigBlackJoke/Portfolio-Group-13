a8229c96b51f7e23aafd552a7445fb620c9fcdbb
d1zzyroma <nataliawologin@gmail.com>
2024-06-16 22:58:41 +0200

    321

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,39 +2,30 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 const accordion = new Accordion('.accordion-container');
-accordion.open(0); 
-
-let lastOpenedItem = document.querySelector('.ac.is-open');
 
 const firstItem = document.querySelector('.ac:first-child');
-firstItem.classList.add('is-open');
-firstItem.style.backgroundColor = 'rgba(0, 176, 104, 0.2)'; 
+
+firstItem.style.backgroundColor = 'rgba(0, 176, 104, 0.2)';
 firstItem.style.borderRadius = '16px';
 
+accordion.open(0);
+
+let lastOpenedItem = firstItem;
+
 const btnEl = document.querySelectorAll('.ac-trigger');
-btnEl.forEach((element, index) => {
+btnEl.forEach(element => {
     element.addEventListener('click', () => {
         const currentItem = element.closest('.ac');
 
-        if (!currentItem.classList.contains('is-open')) {
-            if (lastOpenedItem && !lastOpenedItem.classList.contains('ac:first-child')) {
-                lastOpenedItem.classList.remove('is-open');
-                lastOpenedItem.style.backgroundColor = '';
-                lastOpenedItem.style.borderRadius = '';
-            }
-            lastOpenedItem = currentItem;
+        if (lastOpenedItem !== currentItem) {
+            lastOpenedItem.style.backgroundColor = '';
+            lastOpenedItem.style.borderRadius = '';
         }
 
-        currentItem.classList.toggle('is-open');
-        if (currentItem.classList.contains('is-open')) {
-            currentItem.style.backgroundColor = 'rgba(0, 176, 104, 0.2)';
-            currentItem.style.borderRadius = '16px'; 
-        } else {
-            currentItem.style.backgroundColor = '';
-            currentItem.style.borderRadius = ''
-            
-        }
+        currentItem.style.backgroundColor = 'rgba(0, 176, 104, 0.2)';
+        currentItem.style.borderRadius = '16px'; 
+
+        lastOpenedItem = currentItem;
     });
 });
-
 
