6e743cccbd14424ae421331173a14d60cba9f0e5
BigBlackJoke <155197294+BigBlackJoke@users.noreply.github.com>
2024-06-17 17:43:37 +0300

    dark-mode

--- src/main.js
+++ src/main.js
@@ -4,23 +4,30 @@ import './js/header';
 import './js/reviews';
 import './js/work-together';
 
-// const darkBtn = document.querySelector('.night-mode-checkbox');
+const darkBtn = document.querySelector('.night-mode-checkbox');
+const sections = document.querySelectorAll('section');
 
-// const savedTheme = localStorage.getItem('theme');
+const savedTheme = localStorage.getItem('theme');
 
-// if (savedTheme) {
-//   if (savedTheme === 'dark') {
-//     darkMode.classList.add('dark-mode');
-//     darkBtn.checked = true;
-//   }
-// }
+if (savedTheme) {
+  if (savedTheme === 'dark') {
+    sections.forEach(section => {
+      section.classList.add('dark-mode');
+    });
+    darkBtn.checked = true;
+  }
+}
 
-// darkBtn.addEventListener('change', event => {
-//   if (event.target.checked) {
-//     darkMode.classList.add('dark-mode');
-//     localStorage.setItem('theme', 'dark');
-//   } else {
-//     darkMode.classList.remove('dark-mode');
-//     localStorage.setItem('theme', 'light');
-//   }
-// });
+darkBtn.addEventListener('change', event => {
+  if (event.target.checked) {
+    sections.forEach(section => {
+      section.classList.add('dark-mode');
+    });
+    localStorage.setItem('theme', 'dark');
+  } else {
+    sections.forEach(section => {
+      section.classList.remove('dark-mode');
+    });
+    localStorage.setItem('theme', 'light');
+  }
+});
