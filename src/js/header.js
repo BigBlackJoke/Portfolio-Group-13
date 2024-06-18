fb6d0092c53f59fed28995637669f58621f299a8
BigBlackJoke <155197294+BigBlackJoke@users.noreply.github.com>
2024-06-18 13:58:11 +0300

    ..

--- src/js/header.js
+++ src/js/header.js
@@ -3,27 +3,23 @@ const nav = document.querySelector('.header-nav');
 const navItems = document.querySelectorAll('.nav-menu-item a');
 const darkMode = document.querySelector('.header');
 const darkBtn = document.querySelector('.night-mode-checkbox');
-
 button.addEventListener('click', () => {
   nav.classList.toggle('active');
   button.classList.toggle('active');
 });
-
 navItems.forEach(item => {
   item.addEventListener('click', () => {
     nav.classList.remove('active');
+    button.classList.remove('active');
   });
 });
-
 const savedTheme = localStorage.getItem('theme');
-
 if (savedTheme) {
   if (savedTheme === 'dark') {
     darkMode.classList.add('dark-mode');
     darkBtn.checked = true;
   }
 }
-
 darkBtn.addEventListener('change', event => {
   if (event.target.checked) {
     darkMode.classList.add('dark-mode');
